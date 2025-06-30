b99fc11094ae1e448aea22546d9bd48162cd75e1
Asmita-QA99 <asmita.bramhankar99@gmail.com>
2025-06-30 09:07:34 +0530

    Updated main.java

--- src/main.java
+++ src/main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+    int i = 10;
+    int j = 20;
+    int c = i + j; 
+        System.out.println("Addition of i+j:"   +c);
+    
+    
     }
 }
